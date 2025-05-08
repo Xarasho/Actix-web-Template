24e4948c789b1572869add6c1b6f7134fb775100
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2025-05-08 14:19:10 -0600

    Added the read_task function and its get request

--- src/main.rs
+++ src/main.rs
@@ -97,6 +97,14 @@ async fn create_task(app_state: web::Data<AppState>, task: web::Json<Task>) -> i
     HttpResponse::Ok().finish()
 }
 
+async fn read_task(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl Responder {
+    let mut db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
+    match db.get(&id.into_inner()) {
+        Some(task) => HttpResponse::Ok().json(task),
+        None => HttpResponse::NotFound().finish()
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let db: Database = match Database::load_from_file() {
@@ -123,6 +131,8 @@ async fn main() -> std::io::Result<()> {
             )
             .app_data(data.clone())
             .route("/task", web::post().to(create_task))
+            .route("/task/{id}", web::get().to(read_task))
+
     })
     .bind("127.0.0.1:8080")?
     .run()
